471141f3afbea84c44e12ba2f2396fa3d8fcc111
DavidRG13 <davidrg131092@gmail.com>
2014-03-02 21:05:00 +0100

    DBHelper completed and refactor done

--- PMDM-Tarea4-Renderer/src/com/foc/storage/DbHelper.java
+++ PMDM-Tarea4-Renderer/src/com/foc/storage/DbHelper.java
@@ -7,27 +7,62 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DbHelper extends SQLiteOpenHelper {
 	
-	private static final String DATABASE_NAME = "";
-	private static final int DATABASE_VERSION = 1; 
-	private static final String TABLE_NAME = "";
-	private static final String UID = "_id";
+	private static final String DATABASE_NAME = "store";
+	private static final int DATABASE_VERSION = 1;
+	
+	//SQL commands
+	private static final String CREATE_TABLE = "CREATE TABLE ";
+	private static final String DROP_TABLE_IF_EXISTS = "DROP TABLE IF EXISTS ";
+	private static final String FOREIGN_KEY = "FOREIGN KEY ";
+	private static final String REFERENCES = "REFERENCES ";
+	private static final String PK = "PRIMARY KEY ";
+	private static final String AI = "AUTOINCREMENT ";
+	
+	//Table names
+	private static final String PRODUCT_TABLE = "products";
+	private static final String TOBUY_PRODUCT_TABLE = "tobuy";
+	private static final String BOUGHT_PRODUCT_TABLE = "bought";
+	
+	//Common column names
+	private static final String ID = "_id";
+	private static final String FK_ID = "productCode";
+	
+	//Productos table column names
 	private static final String NAME = "Name";
-	private static final String CREATE_TABLE = "CREATE TABLE "+ TABLE_NAME +" ("
-			+ UID +" INTEGER PRIMARY KEY AUTOINCREMENT,"
+	private static final String PRICE = "Price";
+	private static final String DESC = "Description";
+	private static final String IMG = "image";
+	
+	private static final String CREATE_TABLE_PRODUCT = CREATE_TABLE + PRODUCT_TABLE +" ("
+			+ ID +" INTEGER "+ PK + AI +","
 			+ NAME +" VARCHAR(50),"
+			+ PRICE +" REAL,"
+			+ DESC +" TEXT(255),"
+			+ IMG +" TEXT(20)"
 			+ ");";
-	private Context context;
+	
+	private static final String CREATE_TABLE_TOBUY = CREATE_TABLE + TOBUY_PRODUCT_TABLE +" ("
+			+ ID +" INTEGER "+ PK + AI +","
+			+ FK_ID +" INTEGER,"
+			+ FOREIGN_KEY +"(" + FK_ID +")"+ REFERENCES + PRODUCT_TABLE +"("+ ID +")" 
+			+ ");";
+	
+	private static final String CREATE_TABLE_BOUGHT = CREATE_TABLE + BOUGHT_PRODUCT_TABLE +" ("
+			+ ID +" INTEGER "+ PK +AI +","
+			+ FK_ID +" INTEGER"
+			+ FOREIGN_KEY +"(" + FK_ID +")"+ REFERENCES + PRODUCT_TABLE +"("+ ID +")" 
+			+ ");";
 	
 	public DbHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
-		this.context = context;
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		//TODO completar creación tabla
 		try {
-			db.execSQL(CREATE_TABLE);
+			db.execSQL(CREATE_TABLE_PRODUCT);
+			db.execSQL(CREATE_TABLE_TOBUY);
+			db.execSQL(CREATE_TABLE_BOUGHT);
 		} catch (SQLException e) {
 			//Message.message( context, ""+ e);
 		}
@@ -35,7 +70,11 @@ public class DbHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+		db.execSQL(DROP_TABLE_IF_EXISTS + PRODUCT_TABLE);
+        db.execSQL(DROP_TABLE_IF_EXISTS + TOBUY_PRODUCT_TABLE);
+        db.execSQL(DROP_TABLE_IF_EXISTS + BOUGHT_PRODUCT_TABLE);
+ 
+        onCreate(db);
 	}
 
 }
